c882337b18d3393e146c659d9935b59f617d73ea
Ashunti Ware <ashunti@Ashuntis-Air.lan>
2023-01-17 15:31:06 -0600

    third commit

---
+++ assignments/programmingBasics/loopsalgo/main.js
@@ -0,0 +1,16 @@
+// // Print out all odd numbers from 1 to 20
+// for(i = 1; i<= 20; i++){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+// The expected output will be 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+
+// Sum numbers from 1 to 5, printing out the current number and sum so far at each step of the way
+// // Sum and Print 1-5
+// var sum = 0
+// for (i = 1; i<=5; i++){
+//     sum =+ sum + i 
+//     console.log(i)
+// }
+// The expected output will be: Num: 1, Sum: 1, Num: 2, Sum: 3, Num: 3, Sum: 6, Num: 4, Sum: 10, Num: 5, Sum: 15
