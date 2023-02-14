4846eca4c5b482333bc98b2acd9fc193c5b7999c
Ashunti Ware <ashunti@Ashuntis-MacBook-Air.local>
2023-02-13 19:30:43 -0600

    fundamentals1

---
+++ assignments/cSharp/Fundamentals1/Program.cs
@@ -0,0 +1,52 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// // Create a loop that prints all values from 1 - 255]
+// for(int i = 1; i <= 255; i++)
+//  {
+//     Console.WriteLine(i);
+// }
+
+// // Create a new loop tht generates 5 random numbers between 10 and 20.
+// Random rand = new Random();
+// for(int y = 1; y <= 5; y++) {
+//     Console.WriteLine(rand.Next(10,21));
+// }
+
+// Modify the previous loop to add the random values together and print the sum after the loop finishes.
+
+//Create a new loop that prints all values from 1 to 100 that are divisible by 3 or 5, but not both.
+//  for(int i = 1; i <= 100; i++) 
+//  {
+//     if(i % 3 == 0 && i % 5 == 0) 
+//   {
+//     continue;
+//   }
+ 
+//     if(i % 3 == 0 || i % 5 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     }
+
+// Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for numbers that are multiples of both 3 and 5. 
+for (int i = 1; i <= 100; i++)  
+{  
+        if (i % 3 == 0 && i % 5 == 0)  
+        {  
+            Console.WriteLine("FizzBuzz");  
+        }  
+        else if (i % 3 == 0)  
+        {  
+           Console.WriteLine("Fizz");  
+        }  
+        else if (i % 5 == 0)  
+        {  
+           Console.WriteLine("Buzz");  
+        }  
+        else  
+        {  
+            Console.WriteLine(i);  
+        }  
+}  
+//Modify the previous loop once more to now also print "FizzBuzz" for numbers that are multiples of both 3 and 5. 
